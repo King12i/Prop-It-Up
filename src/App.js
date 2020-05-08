f1ef8f87e873bc753dba4ee68b1f0d31cee71dc3
unknown <ngengwalutteur11@gmail.com>
2020-05-08 16:53:05 -0700

    Prop It Up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import Prop_it_up from './components/Prop_it_up'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1><Prop_it_up  User = {" Doe, Jane"} /></h1>
+     <Prop_it_up User = {"Age: 45"} />
+     <Prop_it_up User = {"Hair Color: Black"} />
+
+     <h1><Prop_it_up  User = {" Smith, John"} /></h1>
+     <Prop_it_up User = {"Age: 85"} />
+     <Prop_it_up User = {"Hair Color: Brown"} />
+
+     <h1><Prop_it_up  User = {" Filmore, Millard"} /></h1>
+     <Prop_it_up User = {"Age: 50"} />
+     <Prop_it_up User = {"Hair Color: Brown"} />
+
+     <h1><Prop_it_up  User = {" Smith, Maria"} /></h1>
+     <Prop_it_up User = {"Age: 65"} />
+     <Prop_it_up User = {"Hair Color: Brown"} />
     </div>
+    
   );
 }
 
---
+++ src/components/Prop_it_up.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Prop_it_up extends Component{
+    render() {
+        return(
+            <div> 
+             {this.props.User}
+            </div>
+        )
+    }
+}
+export default Prop_it_up;
